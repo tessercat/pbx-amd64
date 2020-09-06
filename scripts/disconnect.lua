a9e1b473b6a2fb9d6ff5bff22889711c23ee62a3
Peter Christensen <tessercat@pakaa.net>
2020-09-05 22:48:30 -0600

    Simplify login/disconnect.

--- scripts/disconnect.lua
+++ scripts/disconnect.lua
@@ -1,8 +1,13 @@
 -- freeswitch.consoleLog('INFO', event:serialize())
--- The verto_login field is nil when a client disconnects without logging in first.
-local client_id = event:getHeader('verto_login')
-local gateway = freeswitch.getGlobalVariable('curl_gateway_url')
-local args_tpl = '%s post %s'
-local data_tpl = 'action=verto_client_disconnect&client_id=%s'
-local data = string.format(data_tpl, client_id)
-freeswitch.API():execute('curl', string.format(args_tpl, gateway, data))
+if (event:getHeader('verto_login') == nil) then return end
+freeswitch.API():execute(
+    'curl',
+    string.format(
+        '%s post %s',
+        freeswitch.getGlobalVariable('curl_gateway_url'),
+        string.format(
+            'action=verto_client_disconnect&client_id=%s',
+            event:getHeader('verto_login')
+        )
+    )
+)
--- scripts/login.lua
+++ scripts/login.lua
@@ -1,18 +1,27 @@
 -- freeswitch.consoleLog('INFO', event:serialize())
-local client_id = event:getHeader('verto_login')
-local gateway = freeswitch.getGlobalVariable('curl_gateway_url')
-local args_tpl = '%s post %s'
-local data_tpl = 'action=verto_client_login&client_id=%s&result=%s'
-if (client_id ~= event:getHeader('verto_sessid')) then
-    freeswitch.consoleLog('WARNING', string.format('Login denied - bad sessid %s.', client_id))
-    freeswitch.API():execute('verto_punt', event:getHeader('verto_sessid'))
-    local data = string.format(data_tpl, client_id, 'bad_sessid')
-    freeswitch.API():execute('curl', string.format(args_tpl, gateway, data))
-else
-    local data = string.format(data_tpl, client_id, 'success')
-    local reply = freeswitch.API():execute('curl', string.format(args_tpl, gateway, data))
-    if (reply == 'punt') then
-        freeswitch.consoleLog('WARNING', string.format('Login denied - channel full %s.', client_id))
-        freeswitch.API():execute('verto_punt', event:getHeader('verto_sessid'))
-    end
+if (event:getHeader('verto_login') ~= '1') then return end
+local reply = freeswitch.API():execute(
+    'curl',
+    string.format(
+        '%s post %s',
+        freeswitch.getGlobalVariable('curl_gateway_url'),
+        string.format(
+            'action=verto_client_login&client_id=%s&session_id=%s',
+            event:getHeader('verto_login'),
+            event:getHeader('verto_sessid')
+        )
+    )
+)
+if (reply == 'punt') then
+    freeswitch.consoleLog(
+        'WARNING',
+        string.format(
+            'Login denied for session %s.',
+            event:getHeader('verto_sessid')
+        )
+    )
+    freeswitch.API():execute(
+        'verto_punt',
+        event:getHeader('verto_sessid')
+    )
 end
